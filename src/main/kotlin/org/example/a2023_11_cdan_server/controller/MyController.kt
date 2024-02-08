cbb8f4814dd1c2296a7bd19b5fb16352eda06d02
AnthonyPCPortable <anth06ny@gmail.com>
2024-02-08 11:56:54 +0100

    Correction SiteWeb P2

--- src/main/kotlin/org/example/a2023_11_cdan_server/controller/MyController.kt
+++ src/main/kotlin/org/example/a2023_11_cdan_server/controller/MyController.kt
@@ -1,6 +1,7 @@
 package org.example.a2023_11_cdan_server.controller
 
 import org.example.a2023_11_cdan_server.StudentBean
+import org.example.a2023_11_cdan_server.model.StudentRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,13 +9,54 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class MyController {
 
+    //http://localhost:8080/filter?name=toto&note=12
+    @GetMapping("/filter")
+    fun filter(name: String? = null, note: Int? = null, model: Model): String {
+        println("/filter name$$name note=$note")
+
+
+        val list = StudentRepository.load().filter {
+            //Il n'y a pas de filtre de nom ou le filtre correspond
+            (name == null || it.name?.lowercase() == name.lowercase())
+                    &&
+                    //idem note
+                    (note == null || note == it.note)
+        }
+
+        model.addAttribute("text", "Recherche : name=$name note=$note")
+        model.addAttribute("student", null)
+        model.addAttribute("listStudent", list)
+
+        //Nom du fichier HTML que l'on souhaite afficher
+        return "welcome"
+    }
+
+    //http://localhost:8080/add?name=toto&note=12
+    @GetMapping("/add")
+    fun add(name: String, note: Int, model: Model): String {
+        println("/add name$$name note=$note")
+
+        val student = StudentBean(name, note)
+        StudentRepository.save(student)
+
+        val list = StudentRepository.load()
+
+        model.addAttribute("text", "Ajout de")
+        model.addAttribute("student", student)
+        model.addAttribute("listStudent", list)
+
+        //Nom du fichier HTML que l'on souhaite afficher
+        return "welcome"
+    }
+
+
     //http://localhost:8080/hello
     @GetMapping("/hello")
-    fun testHello(model :Model): String {
+    fun testHello(model: Model): String {
         println("/hello")
 
         val student = StudentBean("Toto", 12)
-        val list =  arrayListOf(
+        val list = arrayListOf(
             StudentBean("Bob", 10),
             StudentBean("Bobby", 18),
             StudentBean("Charles", 20)
@@ -23,7 +65,6 @@ class MyController {
         model.addAttribute("text", "Bonjour")
         model.addAttribute("student", student)
         model.addAttribute("listStudent", list)
-
 
 
         //Nom du fichier HTML que l'on souhaite afficher
---
+++ src/main/kotlin/org/example/a2023_11_cdan_server/model/StudentRepository.kt
@@ -0,0 +1,24 @@
+package org.example.a2023_11_cdan_server.model
+
+import org.example.a2023_11_cdan_server.StudentBean
+
+//Object permet d'écrire un Singleton instantiable qu'une seule fois
+//Il peut être utiliser pour les classes n'ayant que des éléments dans un companion object
+object StudentRepository {
+
+    private val list = ArrayList<StudentBean>()
+
+    //    Si besoin d'un jeu de donnée
+        init {
+            list.add(StudentBean("toto", 12))
+            list.add(StudentBean("toto", 18))
+            list.add(StudentBean("tata", 12))
+            list.add(StudentBean("tata", 14))
+        }
+
+    //Sauvegarde un étudiant
+    fun save(student: StudentBean) = list.add(student)
+
+    //Retourne la liste des étudiants
+    fun load() =  list
+}
